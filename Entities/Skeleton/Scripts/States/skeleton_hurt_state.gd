0e3ba74035da3bfd6d6fecc8df980f852a3a7c6c
Johan Sampedro <j.sampedro-ext@cultura.fr>
2025-11-24 17:55:56 +0100

    feat(damage_label): add damage label system

--- Entities/Skeleton/Scripts/States/skeleton_hurt_state.gd
+++ Entities/Skeleton/Scripts/States/skeleton_hurt_state.gd
@@ -1,5 +1,7 @@
 extends State
 
+const DAMAGE_LABEL: PackedScene = preload("res://UI/DamageLabel/damage_label.tscn")
+
 @export var animation_player: AnimationPlayer
 
 @onready var skeleton: Skeleton = get_owner()
@@ -8,6 +10,11 @@ var animation_finished := false
 
 func enter() -> void:
 	skeleton.stats.health -= skeleton.other_hitbox.damage
+	
+	var damage_label_node: DamageLabel = DAMAGE_LABEL.instantiate()
+	damage_label_node.global_position = skeleton.global_position + Vector2(-25.0, -40.0)
+	damage_label_node.text = str(skeleton.other_hitbox.damage)
+	get_tree().current_scene.call_deferred("add_child", damage_label_node)
 	
 	if !animation_player.animation_finished.is_connected(_on_hurt_animation_finished):
 		animation_player.animation_finished.connect(_on_hurt_animation_finished)
