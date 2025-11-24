f6fb91e76d063e331938811df1f98e0a6f5d0aaf
Johan Sampedro <j.sampedro-ext@cultura.fr>
2025-11-24 17:39:14 +0100

    feat(anchor): add anchor system

--- Scripts/Components/flip_component.gd
+++ Scripts/Components/flip_component.gd
@@ -1,15 +1,29 @@
 class_name FlipComponent extends Node
 
-@export var character: CharacterBody2D
+@export var anchor: Node2D
+
+var character: CharacterBody2D
+
+
+func _ready() -> void:
+	
+	if anchor is CharacterBody2D:
+		character = anchor
+	
+	if anchor.get_parent() is CharacterBody2D:
+		character = anchor.get_parent()
 
 
 func _process(_delta: float) -> void:
+	if not anchor:
+		return
+	
 	if not character:
 		return
 	
 	if character.velocity.x > 0.0:
-		character.scale.x = character.scale.y * 1
+		anchor.scale.x = anchor.scale.y * 1
 	
 	if character.velocity.x < 0.0:
-		character.scale.x = character.scale.y * -1
+		anchor.scale.x = anchor.scale.y * -1
 
