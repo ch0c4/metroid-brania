9184a4b7d9889b3ea8df46ac5f40ac948ef00450
Johan Sampedro <j.sampedro-ext@cultura.fr>
2025-11-24 17:39:35 +0100

    feat(world): update y sort

--- Levels/Scripts/Controllers/labo_controller.gd
+++ Levels/Scripts/Controllers/labo_controller.gd
@@ -4,9 +4,3 @@ const ROCK_SPIKE_AOE: PackedScene = preload("res://Entities/AOE/RockSpike/rock_s
 
 @onready var player: Player = MainInstance.player
 
-
-func _ready() -> void:
-	await get_tree().create_timer(1.0).timeout
-	var aoe: AOE = ROCK_SPIKE_AOE.instantiate()
-	aoe.global_position = player.global_position
-	get_tree().current_scene.add_child(aoe)
--- Levels/Scripts/level.gd
+++ Levels/Scripts/level.gd
@@ -1,4 +1,4 @@
 class_name Level extends Node2D
 
 func _ready() -> void:
-	pass
+	y_sort_enabled = true
